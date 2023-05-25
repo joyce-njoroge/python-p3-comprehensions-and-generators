2eff1cc2e1e4bfa5ebf2ec85651cd7658a45d532
Joyce Njoroge <joycewambuku33@gmail.com>
2023-05-25 12:06:13 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,22 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    evens = []
-    for num in num_list:
-        if num % 2 == 0:
-            evens.append(num)
+    evens = [num for num in num_list if num % 2 == 0]
     return evens
-
 
 def make_exclamation(sentence_list):
     if not sentence_list:
         return []
-    
-    exclamation_list = []
-    for sentence in sentence_list:
-        exclamation_list.append(sentence + "!")
-    
-    return exclamation_list
-    
+    return [sentence + "!" for sentence in sentence_list]
 
+
+    
     
